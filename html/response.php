8f27af05368e3d75005b0e8fade10eee1e6a54d5
RigbyNoCode <bog10dan10@gmail.com>
2024-10-02 14:38:29 -0400

    3.3.6

--- html/response.php
+++ html/response.php
@@ -2,9 +2,17 @@
 <html>
     
 <head>
+    <title> Form Respnse </title>
     <body>
         <p>GET: <?= var_dump($_GET) ?></p>
         <p>POST: <?= var_dump($_POST) ?></p>
+
+        <p> Your Name is: <?= htmlspecialchars ($_POST['name']) ?></p>
+        <p> Your Age is: <?= htmlspecialchars ($_POST['age']) ?></p>
+        <p> Your Card Number: <?= htmlspecialchars ($_POST['Cnbr']) ?></p>
+        <p> Expiration: <?= htmlspecialchars ($_POST['expire']) ?></p>
+        <P> CVV: <?= htmlspecialchars ($_POST['cvv']) ?></p>
+        <p> your consent <?= htmlspecialchars ($_POST['rights']) ?></p>
     </body> 
 </head>
 </html>
