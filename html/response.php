d0710771fc365e015502b52bd6dbed8fc380c1f5
RigbyNoCode <164461561+RigbyNoCode@users.noreply.github.com>
2024-10-16 15:14:01 -0400

    Update response.php

--- html/response.php
+++ html/response.php
@@ -5,8 +5,8 @@
     <title> Form Respnse </title>
     <body>
     <?php
-        $server = "localhost"
-        $username = "bogdan"
+        $server = "localhost";
+        $username = "bogdan";
         $password = "voidnul0";
         $database = "storage";
         $conn = mysqli_connect($server, $username, $password, $database);
@@ -15,13 +15,25 @@
             die("Connection failed: {mysqli_connect_error()}");
         }
         echo "Connected successfully";
+
         
         $sql = "select * from user_data;"; 
+        $result = mysqli_query($conn, $sql);
+        echo mysqli_num_rows($result);
+
+        foreach ($result as $row) {
+            echo "ID: {$row['id']} | ";
+            echo "Name: {$row['user_name']} | ";
+            echo "Age: {$row['age']} | ";
+            echo "Card Number: {$row['card_number']} | ";
+            echo "Expiration: {$row['card_expiration']} | ";
+            echo "CVV: {$row['cvv']}<br>";
+        }
         $sql = "INSERT INTO user_data (user_name, age, card_number, card_expiration, cvv)
 VALUES ($_POST['name'], $_POST['age'], $_POST['Cnbr'], $_POST['expire'], $_POST['cvv'])   );";
 
 mysqli_close($conn);
 ?>
-    </body> 
+</body> 
 </head>
 </html>
