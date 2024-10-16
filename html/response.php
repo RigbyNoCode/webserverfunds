bb30ca739a1ba0c0b54e4e30e93e1c776bacaccd
RigbyNoCode <164461561+RigbyNoCode@users.noreply.github.com>
2024-10-16 14:58:09 -0400

    PHP sql test

--- html/response.php
+++ html/response.php
@@ -4,14 +4,24 @@
 <head>
     <title> Form Respnse </title>
     <body>
+    <?php
+        $servername = "localhost"
+        $username = "bogdan"
+        $password = "voidnul0";
+        $database = "storage";
+        $conn = mysqli_connect($server, $username, $password, $database);
+        // Check for successful connection
+        if (!$conn) {
+            die("Connection failed: {mysqli_connect_error()}");
+        }
+        echo "Connected successfully";
+        
+        $sql = "select * from user_data;"; 
+        $sql = "INSERT INTO user_data (user_name, age, card_number, card_expiration, cvv)
+VALUES ($_POST['name'], $_POST['age'], $_POST['Cnbr'] $_POST['expire'], $_POST['cvv'])   );";
 
-
-        <p> Your Name is: <?= htmlspecialchars ($_POST['name']) ?></p>
-        <p> Your Age is: <?= htmlspecialchars ($_POST['age']) ?></p>
-        <p> Your Card Number: <?= htmlspecialchars ($_POST['Cnbr']) ?></p>
-        <p> Expiration: <?= htmlspecialchars ($_POST['expire']) ?></p>
-        <P> CVV: <?= htmlspecialchars ($_POST['cvv']) ?></p>
-        <p> Your consent: <?= htmlspecialchars ($_POST['rights']) ?></p>
+mysqli_close($conn);
+?>
     </body> 
 </head>
 </html>
