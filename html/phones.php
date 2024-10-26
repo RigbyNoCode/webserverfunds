65565ebdbe78c58ce946102d223e5f3f7f83a17d
RigbyNoCode <164461561+RigbyNoCode@users.noreply.github.com>
2024-10-26 15:06:05 -0400

    Testing, might be finished

---
+++ html/phones.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>DB pull</title>
+</head>
+<body>
+
+
+<?php
+    $server = "localhost";
+    $username = "bogdan";
+    $password = "voidnul0";
+    $database = "storage";
+    $brand = htmlspecialchars($_GET["brand"]);
+    $conn = mysqli_connect($server, $username, $password, $database);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $sql = "select * from phones";
+    $result = mysqli_query($conn, $sql);
+
+    foreach ($result as $row) 
+    {
+        if ($row["brand"] == $brand) 
+        {
+            echo "{$row['brand']} newest phone is the {$row['model']}, it features the {$row[cpu]} chipset.";
+        }
+    }
+
+    mysqli_close($conn);
+?>
