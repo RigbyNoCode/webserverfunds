c16d874fce549a880d6eb6f6c5df2e568145a7c3
RigbyNoCode <164461561+RigbyNoCode@users.noreply.github.com>
2024-11-20 14:44:55 -0500

    aa

--- html/button.php
+++ html/button.php
@@ -5,13 +5,8 @@
 </head>
 <body>
 <?php
-$output = shell_exec("gpio toggle 0");
+ `gpio toggle 0`;
 
-if ($output !== null) {
-    echo "LED toggled successfully!";
-} else {
-    echo "Error toggling LED.";
-}
 ?>
 </body>
 </html>
